c492420746604f9777dcc860888dce7871adcb68
accacheung <36250116+accacheung@users.noreply.github.com>
2018-08-31 23:54:28 -0700

    Requirements and Starter Files

---
+++ starter_code/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "OrqME0zqpvnnU4T7JUxTCXpl4"
+consumer_secret = "NfzM4oWJ948VaH1ZQHzPiqx3qDCEPqKpciFPbrguYZj4S0MuL0"
+access_token = "485283595-dMSYf5ll5ixpyUmiZwkKjqFPKImKX9X2PttTTFWY"
+access_token_secret = "s6OwV3QzWmHZGOGdIWEDHpxC0ArHLnTMHf3w6KIHe6F4c"
